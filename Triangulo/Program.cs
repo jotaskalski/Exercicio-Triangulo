c2d2ad515186de0b80175e8b4868074348454519
Juju <Juju@DESKTOP-NJUO8V5>
2023-04-05 19:52:00 -0300

    Add project files.

---
+++ Triangulo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício Triângulo");
+
+            do
+            {
+                Triangulo triangulo = new Triangulo();
+                Console.Clear();
+                Console.WriteLine("Informe os lados de um triângulo, separado por espaço: ");
+                string ladosTriangulo = Console.ReadLine();
+                String[] lados = ladosTriangulo.Split(" ");
+
+                triangulo.primeiroLado = int.Parse(lados[0]);
+                triangulo.segundoLado = int.Parse(lados[1]);
+                triangulo.terceiroLado = int.Parse(lados[2]);
+
+                triangulo.VerificarTriangulo();
+                Console.ReadLine();
+
+            } while (true);
+        }
+    }
+}
---
+++ Triangulo/Triangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    internal class Triangulo
+    {
+        public int primeiroLado;
+        public int segundoLado;
+        public int terceiroLado;
+
+        public void VerificarTriangulo()
+        {
+            if (primeiroLado < segundoLado + terceiroLado && segundoLado < primeiroLado + terceiroLado && terceiroLado < primeiroLado + segundoLado)
+            {
+                if (primeiroLado == segundoLado && primeiroLado == terceiroLado)
+                {
+                    Console.WriteLine("Triângulo equilátero");
+                }
+                else if(primeiroLado == segundoLado || primeiroLado == terceiroLado || segundoLado == terceiroLado)
+                {
+                    Console.WriteLine("Triângulo isóceles");
+                }
+                else
+                {
+                    Console.WriteLine("Triângulo escaleno");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Triângulo inválido");
+            }
+        }
+    }
+}
